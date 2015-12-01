68a6619ea99fba1c70f9803bb9c9008b7d2d8bdd
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2015-12-01 12:29:01 +0000

    Added mat. views code

---
+++ views.sql
@@ -0,0 +1,20 @@
+CREATE TABLE view_total_incidents_per_ca
+(
+    community_area VARCHAR(30) NOT NULL PRIMARY KEY,
+    total_incidents INT NOT NULL
+);
+
+INSERT INTO view_total_incidents_per_ca
+SELECT community_area, count(*)
+FROM crimes
+GROUP BY community_area;
+
+CREATE TABLE view_total_incidents_per_type (
+    primary_type VARCHAR(30) NOT NULL PRIMARY KEY,
+    total_incidents INT NOT NULL
+);
+
+INSERT INTO view_total_incidents_per_type
+SELECT primary_type, count(*)
+FROM crimes
+GROUP BY primary_type;
